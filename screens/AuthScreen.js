891efad968eb4abde5a04359ca4422dfa2316a5f
ducanh <ducanhmai205@gmail.com>
2017-10-14 07:51:21 +0700

    ok

---
+++ screens/AuthScreen.js
@@ -0,0 +1,35 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text
+} from 'react-native';
+
+class AuthScreen extends Component {
+  render() {
+    return (
+      <View>
+      	<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+		<Text> Auth screennnnnn</Text>
+
+
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+
+});
+
+
+export default AuthScreen;	
